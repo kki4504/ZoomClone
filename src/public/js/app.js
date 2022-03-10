bcb796371ac6f43a2fed33405a11c51919061e62
kki4504 <rhkddlf9876@gmail.com>
2022-03-10 12:53:05 +0900

    SOCKET

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,30 @@
 console.log('hi')
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server!")
+})
+socket.addEventListener("message", (message) => {
+    console.log("Just go this: ", message.data, "form the server")
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server");
+})
+
+setTimeout(() => {
+    socket.send("Hello from the browser!")
+}, 10000)
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    console.log(input.value);
+    input.value = '';
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import express from "express"
+import http from "http";
+import WebSocket from "ws";
+import { homedir } from "os";
 
 const app = express();
 
@@ -9,10 +12,32 @@ app.set("views", __dirname + "/views") // static assets
 // __dirname -> 현재 폴더를 의미
 app.use("/public", express.static(__dirname+"/public"));
 
-const handleReq = (req, res) => {
-    res.render("home")
-}
-app.get("/", handleReq)
+// const handleReq = (req, res) => {
+//     res.render("home")
+// }
+app.get("/", (req, res) => {res.render("home")})
+app.get("/*", (_, res) => {res.redirect("/")});
 
-app.listen(4000);
+const handleListen = () => console.log(`Listening on http://localhost:4000`);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+
+function handleConnection(socket) {
+    console.log(socket)
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("disconnected from the Browser")
+    })
+    socket.on('message', (message) => {
+        console.log(Buffer.from(message, "base64").toString("utf-8"))
+    })
+    socket.send('Hello!')
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(4000, handleListen);
+// app.listen(4000);
 
